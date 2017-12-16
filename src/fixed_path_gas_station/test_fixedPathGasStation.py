7d06ef384159335abc1cdfbbdfa578347a3a73e3
Freya Behrens <freya@fitt-behrens.de>
2017-12-17 00:46:40 +0100

    Fix typo in test

--- src/fixed_path_gas_station/test_fixedPathGasStation.py
+++ src/fixed_path_gas_station/test_fixedPathGasStation.py
@@ -29,7 +29,7 @@ class TestFixedPathGasStation(TestCase):
         res = FixedPathGasStation(path, capacity, startfuel,liter_per_100_km=100)
         print(res)
         self.assertEqual(res.prev, [0, 1, 2])
-        self.assertEqual(res.fill_amount, [1, 1, 0])
+        self.assertEqual(res.fill_liters, [1, 1, 0])
 
     def test_insufficient_capacity(self):
         path = pd.DataFrame({'cost': [3, 2, 1],
