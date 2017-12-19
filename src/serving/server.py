c82ab33ca1637592bd169f35abcdc1c22a0ae04a
Freya Behrens <freya@fitt-behrens.de>
2017-12-19 16:30:07 +0100

    Throw more meaningful errors

--- src/serving/server.py
+++ src/serving/server.py
@@ -8,16 +8,19 @@ import src.serving.route_prediction as prediction
 
 app = Flask(__name__)
 
+respond_error = lambda message: {'error': message}
+algorithm_error = respond_error('Algorithm failed')
+
 
 @app.route('/prediction/berta', methods=['POST'])
 def get_prediction_for_route():
     if request.json is None:
-        return "Error: No JSON has been provided"
+        return jsonify(respond_error("No JSON has been provided"), status=418)
 
     required_keys = {'fuel'}
     missing_keys = set(request.json.keys()) - required_keys
     if missing_keys:
-        return "Error: also expected this key(s): {}".format(missing_keys)
+        return jsonify(respond_error("Also expected this key(s): {}".format(missing_keys)), status=418)
 
     val = request.json
     fuel = val['fuel']
@@ -26,7 +29,12 @@ def get_prediction_for_route():
         return "Error: No FILE containing the route has been uploaded"
 
     f = request.files['route']
-    result = prediction.get_fill_instructions_for_route(f, start_fuel=fuel)
+
+    try:
+        result = prediction.get_fill_instructions_for_route(f, start_fuel=fuel)
+    except Exception as e:
+        print(e)
+        return jsonify(algorithm_error,status=500)
 
     return jsonify(result)
 
@@ -35,12 +43,12 @@ def get_prediction_for_route():
 @app.route('/prediction/google', methods=['POST'])
 def get_prediction():
     if request.json is None:
-        return "Error: No JSON has been provided"
+        return jsonify(respond_error("No JSON has been provided"), status=418)
 
     required_keys = {'length', 'speed', 'fuel', 'capacity'}
     missing_keys = set(request.json.keys()) - required_keys
     if missing_keys:
-        return "Error: also expected this key(s): {}".format(missing_keys)
+        return jsonify(respond_error("Also expected this key(s): {}".format(missing_keys)),status=418)
 
     val = request.json
     path = [(p[0], p[1]) for p in val['path']]
@@ -49,8 +57,13 @@ def get_prediction():
     speed = val['speed']
     fuel = val['fuel']
     capacity = val['capacity']
-    result = prediction.get_fill_instructions_for_google_path(path, path_length_km=length, start_time=start_time,
+    try:
+        result = prediction.get_fill_instructions_for_google_path(path, path_length_km=length, start_time=start_time,
                                                               speed_kmh=speed, capacity_l=capacity, start_fuel_l=fuel)
+    except Exception as e:
+        print(e)
+        return jsonify(algorithm_error,status=500)
+
     return jsonify(result)
 
 
